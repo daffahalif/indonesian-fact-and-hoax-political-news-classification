91d1aeb3def9a5f19d6ff4518b5442bc4efe236b
daffa <dapak@MacBook-Air-daffa.local>
2025-02-24 13:57:45 +0700

    import and combine all separated datasets

---
+++ main_code.py
@@ -0,0 +1,141 @@
+import pandas as pd # For I/O, Data Transformation
+from sklearn.model_selection import train_test_split
+
+## Import data ke dataframe
+data1 = pd.read_excel("archive/Summarized/dataset_cnn_summarized.xlsx")
+data2 = pd.read_excel("archive/Summarized/dataset_kompas_summarized.xlsx")
+data3 = pd.read_excel("archive/Summarized/dataset_tempo_summarized.xlsx")
+data4 = pd.read_excel("archive/Summarized/dataset_turnbackhoax_summarized.xlsx")
+
+df1 = data1[['summarized','label']]
+df2 = data2[['summarized','label']]
+df3 = data3[['summarized','label']]
+df4 = data4[['summarized','label']]
+
+df = pd.concat([df1,df2,df3,df4])
+
+# def hapuss(data):   
+#     if      data.find("(BE") == -1 \
+#         and data.find("(Campuran; Disinformasi, Hasut, & Fakta)") == -1 \
+#         and data.find("(DISINFORMASI") == -1 \
+#         and data.find("(EDUKASI)") == -1 \
+#         and data.find("(EVENTS)") == -1 \
+#         and data.find("(F") == -1 \
+#         and data.find("(H") == -1 \
+#         and data.find("(ISU)") == -1 \
+#         and data.find("(KLARIFIKASI)") == -1 \
+#         and data.find("(MISINFORMASI)") == -1 \
+#         and data.find("(SALAH)") == -1 \
+#         and data.find("[A") == -1 \
+#         and data.find("[B") == -1 \
+#         and data.find("[C") == -1 \
+#         and data.find("[D") == -1 \
+#         and data.find("[E") == -1 \
+#         and data.find("[F") == -1 \
+#         and data.find("[H") == -1 \
+#         and data.find("[I") == -1 \
+#         and data.find("[K") == -1 \
+#         and data.find("[M") == -1 \
+#         and data.find("[R") == -1 \
+#         and data.find("[S") == -1 \
+#         and data.find("[T") == -1 \
+#         and data.find("[U") == -1 \
+#         and data.find("DISINFORMASI :") == -1 \
+#         and data.find("EDUKASI :") == -1 \
+#         and data.find("FITNAH :") == -1 \
+#         and data.find("FITNAH:") == -1 \
+#         and data.find("HASUT :") == -1 \
+#         and data.find("HASUT:") == -1 \
+#         and data.find("HOAX :") == -1 \
+#         and data.find("HOAX:") == -1 \
+#         and data.find("KLARIFIKASI :") == -1 \
+#         and data.find("KLARIFIKASI :") == -1 \
+#         and data.find("MISINFORMASI:") == -1 \
+#         and data.find("SALAH]") == -1 : # data berbahasa inggris tidak dapat dijadikan input
+#         data = data
+#     else:
+#         data = None
+#     return data
+
+# df4 = df4['title'].apply(lambda x : hapuss(x))
+
+# def hl(data): # Hapus label pada judul
+#     if  data.find("(BE") == -1 \
+#         and data.find("(Campuran; Disinformasi, Hasut, & Fakta)") == -1 \
+#         and data.find("(DISINFORMASI") == -1 \
+#         and data.find("(EDUKASI)") == -1 \
+#         and data.find("(EVENTS)") == -1 \
+#         and data.find("(F") == -1 \
+#         and data.find("(H") == -1 \
+#         and data.find("(ISU)") == -1 \
+#         and data.find("(KLARIFIKASI)") == -1 \
+#         and data.find("(MISINFORMASI)") == -1 \
+#         and data.find("(SALAH)") == -1 \
+#         and data.find("[A") == -1 \
+#         and data.find("[B") == -1 \
+#         and data.find("[C") == -1 \
+#         and data.find("[D") == -1 \
+#         and data.find("[E") == -1 \
+#         and data.find("[F") == -1 \
+#         and data.find("[H") == -1 \
+#         and data.find("[I") == -1 \
+#         and data.find("[K") == -1 \
+#         and data.find("[M") == -1 \
+#         and data.find("[R") == -1 \
+#         and data.find("[S") == -1 \
+#         and data.find("[T") == -1 \
+#         and data.find("[U") == -1 \
+#         and data.find("DISINFORMASI :") == -1 \
+#         and data.find("EDUKASI :") == -1 \
+#         and data.find("FITNAH :") == -1 \
+#         and data.find("FITNAH:") == -1 \
+#         and data.find("HASUT :") == -1 \
+#         and data.find("HASUT:") == -1 \
+#         and data.find("HOAX :") == -1 \
+#         and data.find("HOAX:") == -1 \
+#         and data.find("KLARIFIKASI :") == -1 \
+#         and data.find("KLARIFIKASI :") == -1 \
+#         and data.find("MISINFORMASI:") == -1 \
+#         and data.find("SALAH]") == -1 :
+#         data = data
+#     else:
+#         data = data.replace(data.find("(BE"),"")
+#         data = data.replace(data.find("(Campuran; Disinformasi, Hasut, & Fakta)"),"")
+#         data = data.replace(data.find("(DISINFORMASI"),"")
+#         data = data.replace(data.find("(EDUKASI)"),"")
+#         data = data.replace(data.find("(EVENTS)"),"")
+#         data = data.replace(data.find("(F"),"")
+#         data = data.replace(data.find("(H"),"")
+#         data = data.replace(data.find("(ISU)"),"")
+#         data = data.replace(data.find("(KLARIFIKASI)"),"")
+#         data = data.replace(data.find("(MISINFORMASI)"),"")
+#         data = data.replace(data.find("(SALAH)"),"")
+#         data = data.replace(data.find("[A"),"")
+#         data = data.replace(data.find("[B"),"")
+#         data = data.replace(data.find("[C"),"")
+#         data = data.replace(data.find("[D"),"")
+#         data = data.replace(data.find("[E"),"")
+#         data = data.replace(data.find("[F"),"")
+#         data = data.replace(data.find("[H"),"")
+#         data = data.replace(data.find("[I"),"")
+#         data = data.replace(data.find("[K"),"")
+#         data = data.replace(data.find("[M"),"")
+#         data = data.replace(data.find("[R"),"")
+#         data = data.replace(data.find("[S"),"")
+#         data = data.replace(data.find("[T"),"")
+#         data = data.replace(data.find("[U"),"")
+#         data = data.replace(data.find("DISINFORMASI :"),"")
+#         data = data.replace(data.find("EDUKASI :"),"")
+#         data = data.replace(data.find("FITNAH :"),"")
+#         data = data.replace(data.find("FITNAH:"),"")
+#         data = data.replace(data.find("HASUT :"),"")
+#         data = data.replace(data.find("HASUT:"),"")
+#         data = data.replace(data.find("HOAX :"),"")
+#         data = data.replace(data.find("HOAX:"),"")
+#         data = data.replace(data.find("KLARIFIKASI :"),"")
+#         data = data.replace(data.find("KLARIFIKASI :"),"")
+#         data = data.replace(data.find("MISINFORMASI:"),"")
+#         data = data.replace(data.find("SALAH]"),"")
+#     return data
+
+# df4['title'] = df4['title'].apply(lambda x : hl(x))
